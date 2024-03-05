770b9539b7997ecdbd82984719bd5b0b64fc97ab
FredyPot <ftpotter1993@gmail.com>
2024-03-05 18:38:59 +0300

    Initial commit

---
+++ src/main/java/ru/gorshkov/gameother/GameOtherApplication.java
@@ -0,0 +1,13 @@
+package ru.gorshkov.gameother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOtherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameOtherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gorshkov/gameother/GameOtherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gorshkov.gameother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOtherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
